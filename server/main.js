9daf6552a8414ddc8e087dd1ba4b652900a12261
Mani <mani.ebra@gmail.com>
2023-08-28 22:50:28 +0330

    init

---
+++ server/main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+app.use("/css", express.static(__dirname + "./../client/css/"));
+app.use("/html", express.static(__dirname + "./../client/html/"));
+app.use("/js", express.static(__dirname + "./../client/js/"));
+app.use("/img", express.static(__dirname + "./../client/img/"));
+app.use("/fonts", express.static(__dirname + "./../client/fonts/"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "html", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
