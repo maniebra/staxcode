e6d0935600e7d3d71691f238a88e496f774d928b
Mani <mani.ebra@gmail.com>
2023-08-29 12:47:05 +0330

    added functional burger menu for mobile devices

--- client/js/responsive.js
+++ client/js/responsive.js
@@ -1,5 +1,6 @@
 function handlePageSize() {
   var burgerButton = document.getElementById("burger-menu-button");
+
   var toolbar = document.getElementById("toolbar");
   var pageWidth = window.innerWidth;
   var pageHeight = window.innerHeight;
@@ -13,6 +14,26 @@ function handlePageSize() {
     burgerButton.style.display = "none";
     toolbar.style.display = "flex";
   }
+}
+
+function openBurgerMenu() {
+  var burgerMenu = document.getElementById("burger-menu");
+  burgerMenu.style.animationName = "burger-menu-anim-in";
+  burgerMenu.style.animationDuration = "0.6s";
+  burgerMenu.style.animationTimingFunction = "ease-in-out";
+  document.body.style.overflow = "hidden";
+  burgerMenu.style.display = "flex";
+}
+
+function closeBurgerMenu() {
+  var burgerMenu = document.getElementById("burger-menu");
+  document.body.style.overflow = "auto";
+  burgerMenu.style.animationName = "burger-menu-anim-out";
+  burgerMenu.style.animationDuration = "0.6s";
+  burgerMenu.style.animationTimingFunction = "ease-in-out";
+  setTimeout(() => {
+    burgerMenu.style.display = "none";
+  }, 600);
 }
 
 window.onload = function () {
--- server/main.js
+++ server/main.js
@@ -8,6 +8,7 @@ app.use("/html", express.static(__dirname + "./../client/html/"));
 app.use("/js", express.static(__dirname + "./../client/js/"));
 app.use("/img", express.static(__dirname + "./../client/img/"));
 app.use("/fonts", express.static(__dirname + "./../client/fonts/"));
+app.use("/icons", express.static(__dirname + "./../client/icons/"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "html", "index.html"));
